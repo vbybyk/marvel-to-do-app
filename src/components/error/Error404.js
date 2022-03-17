3a59d6c473dba83631cf10b786d3091297d19f07
vbybyk <vitaliy.bybyk@gmail.com>
2022-03-17 12:05:52 +0200

    Updates

--- src/components/error/Error404.js
+++
@@ -1,9 +0,0 @@
-import gif from "./marvel-404-gif.gif";
-
-const ErrorPage = () => {
-  return(
-    <img src={gif} style={{display: "block", overflow: "hidden", width: "550px", height: "260px", objectFit: "cover"}}alt="error"/>
-  )
-}
-
-export default ErrorPage;
