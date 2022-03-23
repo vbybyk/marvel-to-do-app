54d9858397eb42670739fa29d29347292573134e
vbybyk <vitaliy.bybyk@gmail.com>
2022-03-23 10:16:58 +0200

    create: ComicsService.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,8 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -19,7 +21,9 @@ const App = () => {
     <div className="app">
         <AppHeader/>
         <main>
-            <ErrorBoundary>
+            <AppBanner/>
+            <ComicsList/>
+            {/* <ErrorBoundary>
                 <RandomChar/>
             </ErrorBoundary>
             <div className="char__content">
@@ -29,7 +33,7 @@ const App = () => {
                 <ErrorBoundary>
                     <CharInfo charId={activeChar}/>
                 </ErrorBoundary>
-            </div>
+            </div> */}
             <img className="bg-decoration" src={decoration} alt="vision"/>
         </main>
     </div>
---
+++ src/services/ComicsService.js
@@ -0,0 +1,33 @@
+import {useHttp} from '../hooks/http.hooks'
+
+const useComicsService = () => {
+  
+  const {loading, request, error, clearError} = useHttp();
+
+  const _ApiBase = 'https://gateway.marvel.com:443/v1/public';
+  const _ApiKey = 'apikey=c0f7aa699e78f7dd434a9b1612278a6b';
+  const _BasicOffset = 50;
+
+  const getAllComics = async (offset = _BasicOffset) => {
+    const res = await request(`${_ApiBase}/comics?limit=9&offset=${offset}&${_ApiKey}`);
+    return res.data.results.map(_transformComics)
+  }
+  const getSingleComic = async (id) => {
+    const res = await request(`${_ApiBase}/comics/${id}?&${_ApiKey}`);
+    return _transformComics(res.data.results[0])
+  }
+  const _transformComics = (com) => {
+    return {
+        id: com.id,
+        title: com.title,
+        // description: com.description,
+        thumbnail: com.thumbnail.path + '.' + com.thumbnail.extension,
+        homepage: com.urls[0].url,
+        // wiki: com.urls[1].url,
+        prices: com.prices[0].price
+    }
+  }
+  return {loading, error, getAllComics, getSingleComic, clearError}
+}
+
+export default useComicsService;
